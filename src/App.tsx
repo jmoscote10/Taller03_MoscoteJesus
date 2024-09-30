d51e0833742166cc85dd16dec4955b6b5d6bc07c
jmoscote10 <jmoscote10@cuc.edu.co>
2024-09-29 23:34:05 -0500

    Primera versión

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import miImagen from "./assets/img/Logo_Viejo_cuc.ico"
+
+function App() {
+  return (
+    <>
+     <div>
+      <img src={miImagen} alt='logo viejo cuc' />
+      <h1>!!Hola Profe¡¡</h1>
+      <p>Esta es mi segunda actividad usando react...</p>
+     </div>
+    </>
+  );
+}
+
+export default App;
